931d4e75b72e536baac9eac3e777ae217171cb38
Harold Tomlinson <haroldt@creacom.ca>
2024-12-21 19:24:00 -0500

    Created new Github repo on Github site;  created new project in xcode and committing to repo.

---
+++ ccPositionManager/ccPositionManagerApp.swift
@@ -0,0 +1,32 @@
+//
+//  ccPositionManagerApp.swift
+//  ccPositionManager
+//
+//  Created by Harold Tomlinson on 2024-12-21.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct ccPositionManagerApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ ccPositionManagerTests/ccPositionManagerTests.swift
@@ -0,0 +1,16 @@
+//
+//  ccPositionManagerTests.swift
+//  ccPositionManagerTests
+//
+//  Created by Harold Tomlinson on 2024-12-21.
+//
+
+import Testing
+
+struct ccPositionManagerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
