ce87a83220798e2f52f1111e92a763e7f218f0bf
Harold Tomlinson <haroldt@creacom.ca>
2024-12-31 23:58:05 -0500

    working on gui in preparation for api

---
+++ ccPositionManager/WorkflowEnum.swift
@@ -0,0 +1,18 @@
+//
+//  WorkflowEnum.swift
+//  ccPositionManager
+//
+//  Created by Harold Tomlinson on 2024-12-31.
+//
+
+import Foundation
+
+enum WorkflowEnum: String, CaseIterable, Identifiable {
+    var id: Self { self }
+
+    case SellWF
+    case BuyWF
+    case ContractWF
+    case AccountWF
+}
+
