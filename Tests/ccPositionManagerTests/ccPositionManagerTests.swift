0fcd826e8d5466fe4810b7e23fd28e0fc8a47108
Harold Tomlinson <haroldt@creacom.ca>
2025-01-04 22:50:27 -0500

    attempting to add package.swift to get remote CI working

---
+++ Tests/ccPositionManagerTests/ccPositionManagerTests.swift
@@ -0,0 +1,14 @@
+//
+//  ccPositionManagerTests.swift
+//  ccPositionManagerTests
+//
+//  Created by Harold Tomlinson on 2024-12-21.
+//
+
+import Testing
+@testable import ccPositionManager
+
+@Test func example() async throws {
+    // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+}
+
