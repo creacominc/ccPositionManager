462334d09a092d7c6ea77414dcf85550ddf1138b
Harold Tomlinson <haroldt@creacom.ca>
2025-01-12 18:15:48 -0500

    still trying to fix build on github

--- Package.swift
+++ Package.swift
@@ -14,18 +14,20 @@ let package = Package(
             name: "ccPositionManager",
             targets: ["ccPositionManager"]),
     ],
-    // dependencies: [
-    //     .package(name: "Testing", url: "https://github.com/your-repo/Testing.git", from: "1.0.0")
-    // ],
+    dependencies: [
+        // .package(name: "Testing", url: "https://github.com/your-repo/Testing.git", from: "1.0.0")
+        .package(url: "https://github.com/swiftlang/swift-testing.git", branch: "main")
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "ccPositionManager"),
-        .testTarget(
-            name: "ccPositionManagerTests",
-            dependencies: ["ccPositionManager"]
-        ),
+        // .testTarget(
+        //     name: "ccPositionManagerTests",
+        //     dependencies: ["ccPositionManager"],
+        //     .product(name: "Testing", package: "swift-testing")
+        // ),
     ],
     swiftLanguageVersions: [.v5]
 
